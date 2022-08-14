837de1c06841f8f87b646f14c2221a02ef28f9ad
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-08-14 21:03:16 +0100

    Remove python from nix dependencies

--- shell.nix
+++ shell.nix
@@ -1,17 +1,10 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/b58ada326aa612ea1e2fb9a53d550999e94f1985.tar.gz") { }
 }:
-let
-  packages = python-packages: with python-packages; [
-    pyyaml
-  ];
-  python-packages = pkgs.python3.withPackages packages;
-in
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
     pandoc
-    python-packages
     tectonic
-    # Used by some js shit
+    # Used by puppeteer shit
     chromium
     nodejs
     yarn
@@ -27,7 +20,7 @@ pkgs.mkShell rec {
       export PUPPETEER_CFG_PATH="puppeteer-config.json"
       export MMDC=./node_deps/node_modules/.bin/mmdc
 
-      # config
+      # Puppeteer config
       echo "{\"executablePath\": \"$(which chromium)\", \"args\": [\"--no-sandbox\"] }" > ./puppeteer-config.json
     '';
 }
